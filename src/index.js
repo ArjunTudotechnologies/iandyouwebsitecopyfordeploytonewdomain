0277d2564f9c00c171cfb6e5d135938a6c9ae25c
kishanqr <kishanqr@gmail.com>
2020-05-12 21:30:55 +0530

    add routes and basic pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
+// pages
+import HomePage from '../src/pages/Home/Home.js';
+import AboutPage from "../src/pages/About/About.js";
+import ComponentsPage from "../src/pages/UIComponents/UIComponents.js";
+
+var hist = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={hist}>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/about" exact component={AboutPage} />
+      <Route path="/components" exact component={ComponentsPage} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
