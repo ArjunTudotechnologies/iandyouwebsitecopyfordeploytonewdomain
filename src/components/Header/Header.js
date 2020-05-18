1f03cb27663ba3cfb20ad2bc611d632d1013e810
kishanqr <kishanqr@gmail.com>
2020-05-19 05:13:30 +0530

    add mega menu style for navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,21 +13,43 @@ export default class Header extends Component {
                     <Container>
                         <Navbar.Brand href="#home">
                             <img className="logo" alt="I And You Foundation" src={Logo} />
-                            <span className="logo-text">i and you being together foundation</span>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
                                 <Nav.Link href="#home" className="ml-5">Home</Nav.Link>
-
-                                <NavDropdown title="Wisdom" id="basic-nav-dropdown-wisdom">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-
+                                <li class="nav-item px-4 dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="servicesDropdown">
+                                        <a class="dropdown-item" href="#">What we do</a>
+                                        <a class="dropdown-item" href="#">How we fit your needs</a>
+                                        <div class="dropdown-divider"></div>
+                                        <div class="d-md-flex align-items-start justify-content-start">
+                                            <div>
+                                                <div class="dropdown-header">Development</div>
+                                                <a class="dropdown-item" href="#">Bespoke software</a>
+                                                <a class="dropdown-item" href="#">Mobile apps</a>
+                                                <a class="dropdown-item" href="#">Websites</a>
+                                            </div>
+                                            <div>
+                                                <div class="dropdown-header">Professional Services</div>
+                                                <a class="dropdown-item" href="#">Project rescue</a>
+                                                <a class="dropdown-item" href="#">Source code recovery</a>
+                                                <a class="dropdown-item" href="#">Application support &amp; maintenance</a>
+                                            </div>
+                                            <div>
+                                                <div class="dropdown-header">Fixed Price Services</div>
+                                                <a class="dropdown-item" href="#">Add cookie consent</a>
+                                                <a class="dropdown-item" href="#">Add captcha</a>
+                                                <a class="dropdown-item" href="#">Add core data</a>
+                                                <a class="dropdown-item" href="#">Custom error pages</a>
+                                                <a class="dropdown-item" href="#">Contact form creation</a>
+                                                <a class="dropdown-item" href="#">Automated backups</a>
+                                                <a class="dropdown-item" href="#">Image to HTML</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
                                 <NavDropdown title="Yoga & Meditation" id="basic-nav-dropdown-yoga">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
