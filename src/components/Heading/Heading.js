a261a86dd8a7227a311af30f29df4a04b403ce2a
kishanshettyb <kishanqr@gmail.com>
2020-05-26 11:58:15 +0530

    add content for eshwari kriya page

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -4,7 +4,7 @@ export default function Heading(props) {
     return (
         <div className="pb-4">
             <p className="sub-small-title  wow fadeInUp  color-orange" data-wow-delay="0.5s">{props.smalltitle}</p>
-            <h2 className="title  wow fadeInUp" data-wow-delay="1s">{props.title}</h2>
+    <h2 className="title  wow fadeInUp" data-wow-delay="1s">{props.title}<span>{props.specialTitle}</span></h2>
         </div>
     )
 }
